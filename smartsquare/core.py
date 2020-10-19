ce763b4217ff53fe52183cbad9c624e6d2ef6d5e
Federico D'Alba <f.dalba1@studenti.unipi.it>
2020-10-19 16:54:50 +0200

    initial import

---
+++ smartsquare/core.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 19 16:20:08 2020
+
+@author: Uno
+"""
+
+
+def square(x):
+    '''function that return the square '''
+    return x**2.
---
+++ tests/test_core.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 19 16:22:06 2020
+
+@author: Uno
+"""
+
+
+import unittest
+
+from smartsquare.core import square
+
+class TestCore(unittest.TestCase):
+    '''Unittest for core module '''
+
+    def test_float(self):
+        '''test with floats'''
+        self.assertAlmostEqual(square(2.), 4.)
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
